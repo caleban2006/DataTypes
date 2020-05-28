53591934120e737de0578876a26fd7d3942237a6
caleban2006 <logan.jen.2005@gmail.com>
2020-05-28 14:47:44 -0400

    Greet Name added

---
+++ GreetName/GreetName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreetName
+{
+    class GreetName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
