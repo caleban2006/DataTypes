5b694b2df6ef9bd31c9e3ae75b356d7dadb2e3d9
caleban2006 <logan.jen.2005@gmail.com>
2020-05-28 16:27:40 -0400

    added strings

---
+++ Strings/Strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strings
+{
+    class Strings
+    {
+        static void Main(string[] args)
+        {
+            string firstSentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once " +
+                "or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use " +
+                "of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine("Enter the term you would like to search for in Alice in Wonderland?");
+        }
+    }
+}
