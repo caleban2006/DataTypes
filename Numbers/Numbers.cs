e0b3327b86c87c767cbf105370f36a5bb1c21107
caleban2006 <logan.jen.2005@gmail.com>
2020-05-28 15:40:23 -0400

    added rectangle calculations

---
+++ Numbers/Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of the rectangle?");
+            int recatangleLength = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the width of the retangle?");
+            int rectangleWidth = int.Parse(Console.ReadLine());
+
+            int rectangleArea = recatangleLength * rectangleWidth;
+
+            Console.WriteLine("The area of your rectangle is: " + rectangleArea);
+
+        }
+    }
+}
