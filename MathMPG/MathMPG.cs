56006afcdf80d9f8514c94727c6f4524e31e829f
caleban2006 <logan.jen.2005@gmail.com>
2020-05-28 15:46:31 -0400

    added MPG calculation

---
+++ MathMPG/MathMPG.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathMPG
+{
+    class MathMPG
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles have you driven?");
+            int miles = int.Parse(Console.ReadLine());
+            Console.WriteLine("How much gas, in gallons, have you used?");
+            int gas = int.Parse(Console.ReadLine());
+
+            int mpg = miles / gas;
+            Console.WriteLine("You are averaging " + mpg + " miles per gallon.");
+        }
+    }
+}
